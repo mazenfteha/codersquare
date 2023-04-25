8293b1bdcc6901d3b16fdb41d1b665ebe658ba78
mazenfteha <mazn34903@gmail.com>
2023-04-24 20:43:13 -0700

    insert and return posts

--- server/datastore/sql/index.ts
+++ server/datastore/sql/index.ts
@@ -1,4 +1,4 @@
-import { open as sqliteOpen } from 'sqlite';
+import { open as sqliteOpen, Database } from 'sqlite';
 import  sqlite3  from 'sqlite3';
 import path from 'path';
 
@@ -6,17 +6,21 @@ import { Datastore } from "..";
 import { User, Post, Like, Comment } from "../../types";
 
 export class SqlDateStore implements Datastore {
+    private db!: Database<sqlite3.Database, sqlite3.Statement>;
+
     public async openDb() {
         // open the database
-        const db = await sqliteOpen({
+        this.db = await sqliteOpen({
             filename:path.join(__dirname, 'codersquare.sqlite'),
             driver: sqlite3.Database
-        })
+        });
 
-        await db.migrate({
+        this.db.run('PRAGMA foreign_keys = ON;');
+
+        await this.db.migrate({
             migrationsPath:path.join(__dirname, 'migrations'),
         });
-        
+
         return this;
     }
     createUser(user: User): Promise<void> {
@@ -29,10 +33,18 @@ export class SqlDateStore implements Datastore {
         throw new Error("Method not implemented.");
     }
     listPosts(): Promise<Post[]> {
-        throw new Error("Method not implemented.");
+        return this.db.all<Post[]>('SELECT * FROM posts');
     }
-    createPost(post: Post): Promise<void> {
-        throw new Error("Method not implemented.");
+    async createPost(post: Post): Promise<void> {
+        await this.db.run(
+            'INSERT INTO posts (id, title, url, postedAt, userId) VALUES (?,?,?,?,?)',
+            post.id,
+            post.title,
+            post.url,
+            post.postedAt,
+            post.userId
+        );
+        
     }
     getPost(id: string): Promise<Post | undefined> {
         throw new Error("Method not implemented.");
--- server/datastore/sql/migrations/001-initial.sql
+++ server/datastore/sql/migrations/001-initial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id VARCHAR PRIMARY KEY,
     firstName VARCHAR NOT NULL,
-    lastName VARCHAR NOT NULL,
+    lastName VARCHAR NOT NULL, 
     userName VARCHAR UNIQUE NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL
